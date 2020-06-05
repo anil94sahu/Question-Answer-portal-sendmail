0de8858b1b744837efde489dce9681f7f2e83c10
anil94sahu <anil94sahu@gmail.com>
2020-06-05 10:32:49 +0530

    cors origin

--- src/app.js
+++ src/app.js
@@ -8,6 +8,25 @@ const details = require("./details.json");
 
 const app = express();
 app.use(cors({ origin: "*" }));
+
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyParser.json());
 // app.use('/.netlify/functions/api');
 
