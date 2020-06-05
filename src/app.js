0df7e31c21bf969bf0f565b61eda5d10a1b84eb5
anil94sahu <anil94sahu@gmail.com>
2020-06-05 11:57:08 +0530

    checking issue

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const details = require("./details.json");
 
 const app = express();
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 
 app.use(bodyParser.json());
