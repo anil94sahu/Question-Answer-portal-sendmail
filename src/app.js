2ec49afb5ba99eb15de916bf7d01907ce957a912
anil94sahu <anil94sahu@gmail.com>
2020-06-05 10:51:54 +0530

    fixing cors issue

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({ origin: "*" }));
 app.use(function(req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://question-answer-portal.netlify.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
